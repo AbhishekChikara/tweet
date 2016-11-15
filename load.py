08238b53c8b8f8cd58ed8085b681b89c66bd74f5
AbhishekChikara <abhichikara@outlook.com>
2016-11-16 00:50:34 +0530

    Update load.py

--- load.py
+++ load.py
@@ -25,29 +25,3 @@ df = pd.DataFrame(tweets_data, columns= ['text','lang'])
 # Print head of DataFrame
 print(df.head())
 
-# Initialize list to store tweet counts
-['clinton', 'trump', 'machine', 'data'] = [0, 0, 0, 0]
-
-# Iterate through df, counting the number of tweets in which
-# each candidate is mentioned
-for index, row in df.iterrows():
-    clinton += word_in_text('clinton', row['text'])
-    trump += word_in_text('trump', row['text'])
-    machine += word_in_text('machine', row['text'])
-    data += word_in_text('data', row['text'])
-	
-# Import packages
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-
-# Set seaborn style
-sns.set(color_codes=True)
-
-# Create a list of labels:cd
-cd = ['clinton', 'trump', 'sanders', 'cruz']
-
-# Plot histogram
-ax = sns.barplot(cd,[clinton, trump, machine, data])
-ax.set(ylabel="count")
-plt.show()
